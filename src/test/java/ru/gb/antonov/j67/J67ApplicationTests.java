6f37a73b3bdd263f685bb8a2bc6319395f9e27eb
Topsy-Goo <topsygoo@yandex.ru>
2021-08-16 21:09:51 +0300

    1st commit

---
+++ src/test/java/ru/gb/antonov/j67/J67ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.antonov.j67;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J67ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
